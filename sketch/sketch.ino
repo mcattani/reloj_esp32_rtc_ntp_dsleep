477d0a7eb20fb840239e5241ba96930e8bac12a1
mcattani <marianocattani@gmail.com>
2026-05-24 17:20:41 -0300

    agregada lógica principal

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,12 +1,22 @@
+#include <WiFi.h>
 #include "wifi_connect.h"
+#include "display_utils.h"
 
 void setup() {
   Serial.begin(115200);
-
-  // Llamamos a la función para conectar a wifi
-  setup_wifi();
   
+  // Inicializamos el display
+  displayInit();
+  displayStatus("Conectando WiFi...");
 
+  // Intentamos conectar y guardamos el resultado
+  if (setup_wifi()) {
+    // Éxito: Mostramos la IP en pantalla
+    displayInfoNet(WiFi.SSID(), WiFi.localIP().toString());
+  } else {
+    // Fallo: Mostramos error en pantalla
+    displayStatus("Error: No WiFi");
+  }
 }
 
 void loop() {
