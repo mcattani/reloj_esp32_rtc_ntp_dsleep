6d19ae44274efc7c559656e0b9e405ced6c4ead6
mcattani <marianocattani@gmail.com>
2026-05-27 11:25:58 -0300

    actualizado config_example para ser más amigable

--- sketch/config_example.h
+++ sketch/config_example.h
@@ -1,12 +1,14 @@
-/* config_example.h
-Renombrar este archivo a config.h y completar con tus credenciales*/
+/* 
+ * config_example.h
+ * 1. Renombrar este archivo a "config.h".
+ * 2. Completar con tus credenciales de red y preferencias.
+ */
 
-/*#pragma once
+#pragma once
 
+// --- Configuración de Red ---
+const char* WIFI_SSID     = "TU_SSID_AQUI";
+const char* WIFI_PASSWORD = "TU_PASSWORD_AQUI";
 
- // WiFi
- const char* WIFI_SSID = "TU_WIFI_AQUI";
- const char* WIFI_PASSWORD = "TU_PASSWORD_AQUI";
-
- // mDNS
- const char * MDNS_NAME = "NOMBRE_DE_RED_AQUI";/*
+// --- Identidad del Dispositivo ---
+const char* MDNS_NAME     = "esp32-reloj"; // Acceso via http://esp32-reloj.local
